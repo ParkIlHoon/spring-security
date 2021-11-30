9dc288e816f3c27f3c09ef10b999c4ceb6a26ec6
박일훈 <chiwoo2074@gmail.com>
2021-11-30 18:35:17 +0900

    authorization

--- basic-security/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basic-security/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,9 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     UserDetailsService userDetailsService;
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER")
+            .and().withUser("sys").password("{noop}1234").roles("SYS")
+            .and().withUser("admin").password("{noop}1234").roles("ADMIN");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            .antMatchers("/user").hasRole("USER")
+            .antMatchers("/admin").hasRole("ADMIN")
+            .antMatchers("/sys").access("hasRole('ADMIN') or hasRole('SYS')")
             .anyRequest().authenticated();
 
         http
