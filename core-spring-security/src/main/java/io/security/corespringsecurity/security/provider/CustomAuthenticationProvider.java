d14ee392ab3ff4b3e311e19ef1e88def42e2ca53
박일훈 <chiwoo2074@gmail.com>
2021-12-12 22:25:30 +0900

    detail source

--- core-spring-security/src/main/java/io/security/corespringsecurity/security/provider/CustomAuthenticationProvider.java
+++ core-spring-security/src/main/java/io/security/corespringsecurity/security/provider/CustomAuthenticationProvider.java
@@ -1,15 +1,18 @@
 package io.security.corespringsecurity.security.provider;
 
 import io.security.corespringsecurity.domain.Account;
+import io.security.corespringsecurity.security.common.FormWebAuthenticationDetails;
 import io.security.corespringsecurity.security.context.AccountContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.util.StringUtils;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
@@ -27,6 +30,11 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         AccountContext accountContext = (AccountContext) userDetailsService.loadUserByUsername(username);
         if (!passwordEncoder.matches(password, accountContext.getPassword())) {
             throw new BadCredentialsException("bad credential exception");
+        }
+
+        FormWebAuthenticationDetails details = (FormWebAuthenticationDetails) authentication.getDetails();
+        if (!StringUtils.hasText(details.getSecretKey()) || !details.getSecretKey().equals("secret_key")) {
+            throw new InsufficientAuthenticationException("InsufficientAuthenticationException");
         }
 
         Account account = accountContext.getAccount();
